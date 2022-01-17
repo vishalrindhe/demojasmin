f146ba0c1e6abd837c7b6121ec789bc798443ec6
vishalrindhe <rindhevishal32@gmail.com>
2022-01-17 15:51:55 +0530

    abcd

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ app.use(express.static(__dirname + '/dist/demojasmin'));
 // });
 
 app.get('/*',(req, res)=> {
-    res.sendFile('index.html', {root: 'src/'})
+    res.sendFile('index.html', {root: '<name-on-package.json>'})
   });
 app.listen(process.env.PORT || 8080);
