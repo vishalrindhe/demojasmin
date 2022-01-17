8423fcefd878ff986f6183b4ffb897889601eb7d
vishalrindhe <rindhevishal32@gmail.com>
2022-01-17 15:21:10 +0530

    changes

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '/dist/demojasmin'));
+// app.get('/*', function(req,res) {
+//     res.sendFile(path.join(__dirname+'/dist/abcddemo/index.html'));
+// });
+// app.get('*', (req, res) => {
+//     res.sendFile(`./abcd/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+// });
+
+app.get('/*',(req, res)=> {
+    res.sendFile('index.html', {root: 'dist/demojasmin/'})
+  });
+app.listen(process.env.PORT || 8080);
